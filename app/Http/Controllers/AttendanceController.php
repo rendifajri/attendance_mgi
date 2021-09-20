6cc7547fe212c7b25665623d6e804e3a560b2d3b
Rendi Fajrianto <rendi@loyalto.id>
2021-09-21 05:57:49 +0700

    .
    jam 6 pagi after_work_hour harusnya tgl 20 sep, tpi malah 21, padahal harusnya kan blm jam kerjanya

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -143,7 +143,7 @@ class AttendanceController extends Controller
             $diff = (strtotime($date_end) - strtotime($date_start))/3600;
             if(Auth()->user()->employee->shift != 1){//note: cek jika bukan shift 1, maka perlu dicek apakah start di shift tersebut, kurang dari start shift 1, maka date period masuknya di hari kemarinnya.
                 $first_val = $first_work_hour->where("day", $val->day)->first();
-                //$date_period = date("Y-m-d", strtotime("{$date_start} -1 day"));//tunggu pas mau checkin/before_work_hour
+                $date_period = date("Y-m-d", strtotime("{$date_start} -1 day"));//tunggu pas mau checkin/before_work_hour
                 if(strtotime($val->start) < strtotime($first_val->start)){
                     //note: diatas berlaku jika waktu sekarang adalah before untuk shift 3(shift yang kurang start nya kurang dari shift 1), jika before, maka start/end ditambah 1 hari(besoknya), date periodnya hari ini
                     $date_start = date("Y-m-d H:i:s", strtotime("{$date_start} 1 day"));
